083d4703fce0ceb9fc53d20d10630b50f2a4ce03
ankity76 <ankityadav2320@gmail.com>
2024-05-25 21:47:00 +0530

    connect to mongo db

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constansts.js";
+
+import express from 'express';
+const app = express();
+
+
+const connectDB = async() => {
+    try {
+        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+      console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`);
+    
+    } catch (error) {
+        console.error("MONGODB connection ERROR:",error);
+        process.exit(1);
+    }
+} 
+
+export default connectDB;
